d26acc299501827de0b64cc97e9abd18b3d08ac8
Sam ESCOLAS <sescolas@e1z3r2p9.42.us.org>
2017-03-21 09:40:40 -0700

    Changed functionality to take t_dirlist item as input rather than filename in order to address issues with sorting links.

--- srcs/cmp.c
+++ srcs/cmp.c
@@ -6,27 +6,47 @@
 /*   By: sescolas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/09 09:09:56 by sescolas          #+#    #+#             */
-/*   Updated: 2017/03/09 12:31:37 by sescolas         ###   ########.fr       */
+/*   Updated: 2017/03/20 18:10:52 by sescolas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-#include <stdio.h>
-#include <time.h>
 
-int		cmp_time_modified(const char *fp1, const char *fp2)
+static struct stat	get_stat(char *path, char *name)
 {
-	struct stat f1_stat;
-	struct stat	f2_stat;
-	long		t1;
-	long		t2;
+	char		*full_path;
+	struct stat	ret;
 
-	if (stat(fp1, &f1_stat) < 0 || stat(fp2, &f2_stat) < 0)
-		return (0);
-	t1 = f1_stat.st_mtime;
-	t2 = f2_stat.st_mtime;
-	if (t1 == t2)
-		return (ft_strcmp(fp1, fp2));
+	full_path = ft_strjoin(path, name);
+	if (lstat(full_path, &ret) < 0)
+		return (ret);
+	ft_strdel(&full_path);
+	return (ret);
+}
+
+int		cmp_time_modified(t_dirlist *f1, t_dirlist *f2)
+{
+	struct stat		f1_stat;
+	struct stat		f2_stat;
+	struct timespec	t1;
+	struct timespec	t2;
+
+	f1_stat = get_stat(f1->path, f1->dir->d_name);
+	f2_stat = get_stat(f2->path, f2->dir->d_name);
+	t1 = f1_stat.st_mtimespec;
+	t2 = f2_stat.st_mtimespec;
+	if (f1_stat.st_mtime == f2_stat.st_mtime)
+	{
+		if (t1.tv_nsec == t2.tv_nsec)
+			return (ft_strcmp(f2->dir->d_name, f1->dir->d_name));
+		else
+			return (t2.tv_nsec - t1.tv_nsec);
+	}
 	else
-		return (t1 > t2 ? -1 : 1);
+		return (f2_stat.st_mtime - f1_stat.st_mtime);
+}
+
+int		cmp_filename(t_dirlist *f1, t_dirlist *f2)
+{
+	return (ft_strcmp(f1->dir->d_name, f2->dir->d_name));
 }
