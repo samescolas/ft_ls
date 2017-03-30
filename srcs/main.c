3232db5229b4843b0de9c74bfa417d810fdc9f67
Sam ESCOLAS <sescolas@e1z2r3p34.42.us.org>
2017-03-30 16:23:05 -0700

    Moved code from main.c to print2.c

--- srcs/main.c
+++ srcs/main.c
@@ -12,85 +12,24 @@
 
 #include "ft_ls.h"
 
-static void		print_path(char *path)
+static void		cleanup(t_btree *tree, t_lengths *maxes)
 {
-	write(1, path, ft_strlen(path));
-	write(1, ":\n", 2);
-}
-
-static void		recurse(t_btree *node, t_ops ops, t_lengths *empty)
-{
-	if (ft_strcmp(node->d_name, ".") && 	\
-			ft_strcmp(node->d_name, "..") &&
-			is_dir(node->path) && !empty)
-	{
-		write(1, "\n", 1);
-		print_contents(node->path, ops, FALSE);
-	}
-}
-
-static void		display_file(t_btree *node, t_ops ops, t_lengths *empty)
-{
-	static int	first_func_call;
-	t_lengths	*maxes;
-
-	if (is_dir(node->path))
-	{
-		if (ops.multiple_files && first_func_call== 424242)
-			write(1, "\n", 1);
-		if (ops.multiple_files || empty)
-			print_path(node->path);
-		print_contents(node->path, ops, TRUE);
-	}
-	else if (ops.l || ops.g)
-	{
-		maxes = get_max_lengths(node, ops);
-		print_long(node->path, ops, *maxes);
-		ft_padstr(node->path, 0, (ops.color ? BLU : (void *)0));
-		write(1, "\n", 1);
-		ft_bzero(maxes, sizeof(t_lengths));
-		free(maxes);
-	}
-	else
-	{
-		write(1, node->path, ft_strlen(node->path));
-		write(1, "\n", 1);
-	}
-	first_func_call = 424242;
-}
-
-void			print_contents(char *path, t_ops ops, t_bool one_and_only)
-{
-	t_btree			*tree;
-	t_lengths		*maxes;
-	char			*full_path;
-
-	tree = (void *)0;
-	maxes = init_max_lengths();
-	if (path[0] != '.' && path[0] != '/')
-		full_path = ft_strjoin("./", path);
-	else
-		full_path = ft_strdup(path);
-	if (!one_and_only)
-		print_path(full_path);
-	tree = scan_directory(full_path, ops, maxes);
-	print_tree(tree, ops, *maxes);
-	if (ops.recurse)
-		btree_apply_infix(tree, ops, (void *)0, &recurse);
-	ft_strdel(&full_path);
+	uproot(&tree);
 	ft_bzero(maxes, sizeof(t_lengths));
 	free(maxes);
-	uproot(&tree);
+	maxes = (void *)0;
 }
 
 int				main(int argc, char **argv)
 {
-	t_btree	*args;
-	t_ops	*ops;
-	int		i;
+	t_btree		*args;
+	t_ops		*ops;
+	t_lengths	*maxes;
+	int			i;
 
 	args = (void *)0;
 	ops = init_ops();
+	maxes = (void *)0;
 	i = 0;
 	while (++i < argc && argv[i][0] == '-')
 		add_options(argv[i], ops);
@@ -101,8 +40,14 @@ int				main(int argc, char **argv)
 	ops->multiple_files = (argc - i > 1);
 	if (args)
 	{
-		btree_apply_infix(args, *ops, (void *)0, &display_file);
+		maxes = get_max_lengths(args, *ops);
+		btree_apply_infix(args, *ops, maxes, &display_file);
+		cleanup(args, maxes);
+		/*
 		uproot(&args);
+		ft_bzero(maxes, sizeof(*maxes));
+		free(maxes);
+		*/
 	}
 	ft_bzero(ops, sizeof(t_ops));
 	free(ops);
